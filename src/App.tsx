5c3d020d01e8be8a25bd01f0eba1d7f1162dfca2
Danim Kim <110408177+kdndevkr@users.noreply.github.com>
2022-08-19 17:25:46 +0900

    🎉 string 데이터 binary array로 변환시키는 로직 구현

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  function sendPacket(packetName: string, data: any) {
+    const headerSizeBytes = 4; // packetname size 2byte + data size 2byte
+    const stringifiedData = JSON.stringify(data);
+
+    const encoder = new TextEncoder();
+    const packetNameUint8Arr = encoder.encode(packetName);
+    const dataUint8Arr = encoder.encode(stringifiedData);
+    const pktNameLength = packetNameUint8Arr.length;
+    const dataLength = dataUint8Arr.length;
+    const headerUint8Arr = new Uint8Array(headerSizeBytes);
+    headerUint8Arr[0] = pktNameLength;
+    headerUint8Arr[1] = pktNameLength >> 8;
+    headerUint8Arr[2] = dataLength;
+    headerUint8Arr[3] = dataLength >> 8;
+
+    const arrayBufferLength = headerSizeBytes + pktNameLength + dataLength;
+    const byteArray = new ArrayBuffer(arrayBufferLength);
+    let uintArray = new Uint8Array(byteArray);
+
+    uintArray[0] = pktNameLength;
+    uintArray[1] = pktNameLength >> 8;
+    uintArray[2] = dataLength;
+    uintArray[3] = dataLength >> 8;
+
+    for (let i = 0; i < pktNameLength; i++) {
+      uintArray[headerSizeBytes + i] = packetNameUint8Arr.at(i);
+    }
+
+    for (let i = 0; i < dataLength; i++) {
+      uintArray[headerSizeBytes + pktNameLength + i] = dataUint8Arr.at(i);
+    }
+    console.log("uintArray", uintArray);
+    return uintArray;
+  }
+
+  let pktname = "EnterGame";
+
+  const dummyData = {
+    Playerid: "testuser",
+    CurrentMap: "main",
+  };
+
+  const onConnectClick = () => {
+    let socket = new WebSocket("wss://socketsbay.com/wss/v2/2/demo/");
+    const byteArray = sendPacket(pktname, dummyData);
+    socket.onopen = function () {
+      setMessage("[open] 커넥션이 만들어졌습니다.");
+      socket.send(byteArray);
+    };
+  };
+
+  return (
+    <div className="App">
+      <button type="button" onClick={onConnectClick}>
+        Connect
+      </button>
+      <button type="button">Disconnect</button>
+      <p id="messages">{message}</p>
+    </div>
+  );
+}
+
+export default App;
